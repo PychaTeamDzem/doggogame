67ee513a68ba8b715f06d04ae08312ac972190ca
Klaudyna <mikolajczyk.klaudyna@gmail.com>
2020-04-01 23:39:49 +0200

    changes in controller saved

--- Mydoggo/Source/Mydoggo/DoggoAIController.cpp
+++ Mydoggo/Source/Mydoggo/DoggoAIController.cpp
@@ -1,8 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "DoggoAIController.h"
-
 
 #include "Perception/AIPerceptionComponent.h"
 #include "Perception/AISenseConfig_Sight.h"
